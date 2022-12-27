8242b1b2c7b6c7791e34b4701963a4ffff56efb7
jaya-sri-j <jayasrijagan28@gmail.com>
2022-12-27 10:22:39 +0530

    adding files.

---
+++ switch statement/js/main.js
@@ -0,0 +1,14 @@
+var a = 2;
+switch (a) 
+{
+ case 1:
+        a = "one";
+        break;
+ case 2:
+        a ="two";
+        break;
+ default:
+        a = "not found";
+        break;
+}
+alert("the value is"+" "+a);
